c1f1bb2de46ffc92f0c1423cfc5e4207a8027817
Aadhar Chauhan <97212510+Aadharc@users.noreply.github.com>
2023-10-01 14:19:24 -0700

    Added Encoder block

--- model.py
+++ model.py
@@ -108,3 +108,25 @@ class MultiHeadAttention(nn.Module):
 
         #(batch, seq_len, d_model) --> (batch, seq_len, d_model)
         return self.w_o(x)
+    
+class ResidualConnection(nn.Module):
+    "Add and Norm in Transformer Encoder"
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+    
+class EncoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttention, feed_forward: FeedForward, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
